f93c92a7689e2c85ae15562990282224e6cd5847
Quan Ngo <quanngovan98@gmail.com>
2020-06-02 22:33:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Scale down the child when click
+**Example**
+```
+ScaledownButton(
+  child: Icon(
+    Icons.mail_outline,
+    color: Colors.grey,
+    size: 25,
+  ),
+)
+```
+
+result:
+
+![](demo.gif)
---
+++ lib/scaledownbutton.dart
@@ -0,0 +1,56 @@
+library scaledownbutton;
+
+import 'package:flutter/material.dart';
+
+class ScaleDownButton extends StatefulWidget {
+  final Widget child;
+  final Function onTap;
+  final double scale;
+
+  const ScaleDownButton({Key key, this.child, this.onTap, this.scale = 0.1})
+      : super(key: key);
+  @override
+  _ScaleDownButtonState createState() => _ScaleDownButtonState();
+}
+
+class _ScaleDownButtonState extends State<ScaleDownButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 100));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      child: GestureDetector(
+        onTapDown: (detail) {
+          controller.forward();
+        },
+        onTapUp: (detail) {
+          Future.delayed(Duration(milliseconds: 200), () {
+            controller.reverse();
+            if (widget.onTap != null) {
+              widget.onTap();
+            }
+          });
+        },
+        onTapCancel: () {
+          controller.reverse();
+        },
+        child: widget.child,
+      ),
+      builder: (context, child) {
+        return Transform.scale(
+          scale: 1 - controller.value * widget.scale,
+          child: child,
+        );
+      },
+    );
+  }
+}
